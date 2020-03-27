b52cf0e1b5958c2505e90d84b6c797ed22c22010
PedroHPSilvino <phsilvino.7789@hotmail.com>
2020-03-27 14:43:20 -0300

    Tests

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -48,7 +48,7 @@ module.exports = {
             .select('ong_id')
             .first();
         
-        if (incident.ong_id != ong_id){
+        if (incident.ong_id !== ong_id){
             return Response.status(401).json({ error: 'Operation not permitted.'});
         }
 
